16bea8629095cee1e2fb51bcfb747fa83bd7e04c
Cassey R. <rigodon.cv@obmontessori.edu.ph>
2026-03-03 07:56:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-function seeDiscount() {}
-    let budget = document.getElementById("budget").value;
+function seeDiscount() {
+
+    let budget = document.getElementById("priceRange").value;
     let discounted = budget * 0.20;
 
-    document.getElementById("discount").innerHTML = `
-            <button class="btn btn-light mb-2">Brand & Discounts</button>
-
+    document.getElementById("discountDisplay").innerHTML = `
                 <div class="card discount-card p-4 text-center">
 
                 <h5>Recommended Brands:</h5>
@@ -13,4 +12,11 @@ function seeDiscount() {}
                     <button class="btn btn-outline-light btn-sm">Apple</button>
                     <button class="btn btn-outline-light btn-sm">Lenovo</button>
 
+                    <br><br>
+
+                    <p>Eligible Discount: <span class="badge bg-light">${discounted}</span></p>
+                </div>
+
                 `;
+
+}
